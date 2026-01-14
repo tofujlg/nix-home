7ad6cf3e3e4e77e29dcc265e1dbfd8f0fd89fdd9
jujekebab <tofuredbull@gmail.com>
2026-01-14 18:42:26 +0900

    install kanata(not configured)

--- home.nix
+++ home.nix
@@ -22,6 +22,7 @@
     # # "Hello, world!" when run.
     # pkgs.hello
     pkgs.cowsay
+    pkgs.kanata
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
